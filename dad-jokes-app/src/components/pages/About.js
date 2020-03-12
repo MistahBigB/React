51f164e538087849415d167cbde70b483eee0f48
Bigby <bigby@DESKTOP-E37J67C.localdomain>
2020-03-12 16:50:38 -0700

    readme.md

---
+++ dad-jokes-app/src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return(
+        <div className='mx-auto'>
+            <h1>This is a React JS Application</h1>
+            <p>This tutorial covers React Hooks, functional components, and React Router</p>
+        </div>
+    )
+}
